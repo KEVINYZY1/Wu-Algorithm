e1107a1734140cdce87c9f2d17491688768aaacf
wuzhenzhong <jt26wzz@icloud.com>
2016-09-14 16:35:49 +0800

    全排列问题

---
+++ Leetcode/046_Permutations.cc
@@ -0,0 +1,56 @@
+#include<vector>
+using namespace std;
+
+//回溯法
+class Solution1 {
+public:
+    vector<vector<int> > permute(vector<int>& nums) {
+        int len=nums.size();
+        makePermutations(0,len-1, nums);
+        return rec;
+    }
+private:
+    void makePermutations(int low,int high,vector<int>& nums){
+        if(low==high){
+            rec.push_back(nums);
+        }
+        else{
+            for(int i=low;i<=high;i++){
+                swap(nums[i],nums[low]);
+                makePermutations(low+1,high,nums);
+                swap(nums[i],nums[low]);//必须要有这步，一个分支走完了，再返回，继续走
+            }
+        }
+    }
+    vector<vector<int> >rec;
+};
+
+
+//更容易理解
+class Solution2 {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int>> allPer;
+        if(num.empty()) return allPer;
+        vector<bool> used(num.size(), false);
+        vector<int> per;
+        findPermutations(num, used, per, allPer);
+        return allPer;
+    }
+private:
+    void findPermutations(vector<int> &num, vector<bool> &used, vector<int> &per, vector<vector<int>> &allPer) {
+        if(per.size()==num.size()) {
+            allPer.push_back(per);
+            return;
+        }
+        
+        for(int i=0; i<num.size(); i++) {
+            if(used[i]) continue;
+            used[i] = true;
+            per.push_back(num[i]);
+            findPermutations(num, used, per, allPer);
+            used[i] = false;
+            per.pop_back();
+        }
+    }
+};
---
+++ Leetcode/047_PermutationsII.cc
@@ -0,0 +1,56 @@
+#include<vector>
+using namespace std;
+
+class Solution1 {
+public:
+    vector<vector<int> > permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        makePermutations(nums, 0, nums.size()-1);
+        return rec;
+    }
+    
+private:
+    vector<vector<int> > rec;
+    void makePermutations(vector<int> nums, int low, int high) {
+        if (low == high) {
+            rec.push_back(nums);
+            return;
+        }
+        for (int k = low; k <= high; k++) {
+            if (low != k && nums[low] == nums[k]) continue;
+            swap(nums[low], nums[k]);
+            makePermutations(nums, low+1, high);
+            //swap(nums[low], nums[k]);假如这样回溯回去，会出现很多重复的排列。
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int>> allPer;
+        if(num.empty()) return allPer;
+        sort(num.begin(),num.end());
+        vector<int> per;
+        vector<bool> used(num.size(),false);
+        findPerUniq(num, used, per, allPer);
+        return allPer;
+    }
+    
+    void findPerUniq(vector<int> &num, vector<bool> &used, vector<int> &per, vector<vector<int>> &allPer) {
+        if(per.size()==num.size()) {
+            allPer.push_back(per);
+            return;
+        }
+        
+        for(int i=0; i<num.size(); i++) {
+            if(used[i]) continue;
+            if(i>0 && num[i]==num[i-1] && !used[i-1]) continue;
+            used[i] = true;
+            per.push_back(num[i]);
+            findPerUniq(num, used, per, allPer);
+            per.pop_back();
+            used[i] = false;
+        }
+    }
+};
