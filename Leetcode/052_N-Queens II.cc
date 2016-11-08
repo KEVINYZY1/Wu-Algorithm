7c052768fa6cbe4c3d5d4fdecc821b762b72a3b8
wuzhenzhong <jt26wzz@icloud.com>
2016-11-08 09:10:38 +0800

    皇后问题

---
+++ Leetcode/052_N-Queens II.cc
@@ -0,0 +1,40 @@
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int totalNQueens(int n) {
+        int result=0;
+        vector<string> solution(n,string(n,'.'));
+        vector<int> site;//用一个数组记录安放Q的位置,这样可以快速判断是否可以安放皇后
+        findNQueues(n, 0, site, solution, result);
+        return result;
+    }
+private:
+    void findNQueues(int n, int row, vector<int> &site, vector<string> &solution, int &result){
+        if(n==row){
+            result++;
+            return;
+        }
+        for(int i=0; i<solution.size(); i++){
+            if(solution[row][i]=='.'){
+                solution[row][i]='Q';
+                site.push_back(i);
+                if(isNoAttack(site, solution, row, i)){
+                    findNQueues(n,row+1, site, solution, result);
+                }
+                solution[row][i]='.';
+                site.pop_back();
+            }
+        }
+        return;
+    }
+    bool isNoAttack(vector<int> &site, vector<string> &solution, int x, int y){
+        for(int i=0; i<site.size()-1; i++) {
+            if(y==site[i] || abs(x-i)==abs(y-site[i]))
+                return false;
+        }
+        return true;
+    }
+};
